a2c1c4b64aa73d24c993dce249b3b06fb039bae0
Pablo Leta <pablo.200000000@gmail.com>
2022-10-27 19:39:10 -0500

    Agregar clase de operaciones

---
+++ numero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Class1
+{
+	private int num1;
+	private int num2;
+
+	public Class1()
+	{
+
+	}
+
+    public global::System.Int32 Num1 { get => num1; set => num1 = value; }
+    public global::System.Int32 Num2 { get => num2; set => num2 = value; }
+
+	public int Suma()
+    {
+		return num1 / num2;
+    }
+}
